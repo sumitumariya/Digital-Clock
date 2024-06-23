3d3e90f683a68857a68c189f8589ebbdf15c7d4f
sumitumariya <sumitumariya11@gmail.com>
2024-06-23 20:03:24 +0530

    digital_clock.py

--- digital_clock.py
+++ digital_clock.py
@@ -6,13 +6,13 @@ from time import strftime
 root=tk.Tk()
 root.title("Digital Clock")
 
-def time():
-    string = strftime('%H : %M : %S %p \n%D')
-    label.config(text=string)
-    label.after(1000,time)
+# def time():
+#     string = strftime('%H : %M : %S %p \n%D')
+#     label.config(text=string)
+#     label.after(1000,time)
 
-label = tk.Label(root, font=('calibri',50 ,'bold'),background='yellow', foreground ='black')
-label .pack(anchor='center')
+# label = tk.Label(root, font=('calibri',50 ,'bold'),background='yellow', foreground ='black')
+# label .pack(anchor='center')
 
-time()
-root.mainloop()
+# time()
+# root.mainloop()
